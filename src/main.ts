ab3e8d8a57f3dbcdf339a711b7c7d1def6e0eed1
Dobbymin <gmin0701@knu.ac.kr>
2025-09-05 18:46:17 +0900

    refactor: SwaggerModule.setup에서 옵션 객체를 사용하여 설정 간소화

--- src/main.ts
+++ src/main.ts
@@ -38,15 +38,7 @@ async function bootstrap() {
     explorer: false,
     customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
   };
-  SwaggerModule.setup(`api`, app, document, {
-    explorer: false,
-    customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
-    customCssUrl: '/swagger/swagger-ui.css',
-    customJs: [
-      '/swagger/swagger-ui-bundle.js',
-      '/swagger/swagger-ui-standalone-preset.js',
-    ],
-  });
+  SwaggerModule.setup(`api/`, app, document, options);
 
   await app.listen(12345);
 }
